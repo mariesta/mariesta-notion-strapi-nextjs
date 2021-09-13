8dcef91385e45994bc33ebe5c93533eebb121d9c
M. Starck <marie.compsci14@gmail.com>
2021-09-13 20:31:48 +0200

    Cleanup

--- nextjs-frontend/lib/graphql-client.js
+++ nextjs-frontend/lib/graphql-client.js
@@ -15,17 +15,6 @@ function createClient(initialState) {
 export function initializeGraphQL(initialState = null) {
   const _graphQLClient = graphQLClient ?? createClient(initialState);
 
-  // After navigating to a page with an initial GraphQL state, create a new cache with the
-  // current state merged with the incoming state and set it to the GraphQL client.
-  // This is necessary because the initial state of `memCache` can only be set once
-  if (initialState && graphQLClient) {
-    graphQLClient.cache = memCache({
-      initialState: Object.assign(
-        graphQLClient.cache.getInitialState(),
-        initialState
-      ),
-    });
-  }
   // For SSG and SSR always create a new GraphQL Client
   if (typeof window === "undefined") return _graphQLClient;
   // Create the GraphQL Client once in the client
--- nextjs-frontend/pages/index.js
+++ nextjs-frontend/pages/index.js
@@ -1,27 +1,11 @@
-import { initializeGraphQL } from "../lib/graphql-client";
-import graphQLRequest from "../lib/graphql-request";
-import App from "../components/app";
 import PageList from "../components/page-list";
 
-import { ALL_PAGES_QUERY } from "../lib/graphql-query-mutation";
-
 export default function Home() {
-  return (
-    <App>
-      <PageList />
-    </App>
-  );
+  return <PageList />;
 }
 
 export async function getStaticProps() {
-  const client = initializeGraphQL();
-
-  await graphQLRequest(client, ALL_PAGES_QUERY);
-
   return {
-    props: {
-      initialGraphQLState: client.cache.getInitialState(),
-    },
-    revalidate: 1,
+    props: {},
   };
 }
